1001604f530eff6de9c3b69431ed04e31d8a323b
Jonas <jonas.rodriguez@outlook.com>
2019-05-09 06:44:40 -0700

    Added file feature.

--- src/ProcessFile.cpp
+++ src/ProcessFile.cpp
@@ -4,13 +4,20 @@
 #include <regex>
 
 ProcessFile::ProcessFile(std::string file) {
+  int lineCount = 0;
   std::ifstream infile;
   infile.open(file);
   std::string line;
   while (std::getline(infile, line)) {
     ProcessLine(line);
+    lineCount++;
   }
   infile.close();
+
+  for (auto n : names_) {
+    std::cout << n.first << " " << n.second << std::endl;
+  }
+  std::cout << "c: " << lineCount << std::endl;
 }
 
 void ProcessFile::ProcessLine(const std::string &line) {
@@ -32,6 +39,10 @@ void ProcessFile::ProcessLine(const std::string &line) {
 
 void ProcessFile::StoreName(const std::string &name) {
   std::map<std::string, int>::iterator it;
-  if (names_.count(name) == 0) {
+  it = names_.find(name);
+  if (it != names_.end()) {
+    it->second++;
+  } else {
+    names_.insert(std::pair<std::string, int>(name, 1));
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,9 @@
-#include <fstream>
-#include <iostream>
-#include <regex>
-#include <sstream>
-#include <string>
+#include "ProcessFile.h"
 
 using namespace std;
 
-void ProcessLine(const std::string &line) {
-
-  std::string dateTime;
-  std::string name;
-  std::string text;
-
-  // Use regex to split date&time - user - message
-  std::regex re("(.*)\\-(.*)\\:(.*)");
-  std::smatch match;
-  if (std::regex_search(line, match, re) && match.size() > 1) {
-    dateTime = match.str(1);
-    name = match.str(2);
-    text = match.str(3);
-  }
-
-  std::cout << name << std::endl;
-}
-
 int main() {
-  ifstream infile;
-  infile.open("chat.txt");
+  ProcessFile file("chat2.txt");
 
-  std::string line;
-  while (std::getline(infile, line)) {
-    std::istringstream iss(line);
-  }
-
-  infile.close();
   return 0;
 }
